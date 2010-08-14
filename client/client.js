13155421dad0ba8bccd53ba3b5ee0bbe3af1086b
Brent Fitzgerald <burnto@gmail.com>
2010-08-14 16:50:44 -0700

    some styles added, drag/drop in progress

--- client/client.js
+++ client/client.js
@@ -1,3 +1,5 @@
+// Set up websocket
+
 if(("WebSocket" in window)) {
   var ws = new WebSocket("ws://localhost:8125");
   
@@ -20,3 +22,54 @@ if(("WebSocket" in window)) {
 }else{
   alert("This app requires websockets. Please use a browser that supports them!")
 }
+
+$(function() {
+  // Set up dropzone
+  
+  function cancel(event) {
+    if (event.preventDefault) {
+      event.preventDefault();
+    }
+    return false;
+  }
+  
+  var drop = $(".drop");
+  drop.bind('dragover', function(evt) {
+    cancel(evt);
+  })
+  drop.bind('dragenter', function(evt) {
+    drop.find(".message").html("drop me here");
+    cancel(evt);
+  })
+  drop.bind('dragend', function(evt) {
+    console.log('leave');
+    cancel(evt);
+  })
+  drop.bind('drop', function(evt) {
+    this.innerHTML += '<p>' + evt.dataTransfer.getData('Text') + '</p>';
+    cancel(evt);
+  })
+
+  // 
+  // addEvent(drop, 'dragover', cancel);
+  // addEvent(drop, 'dragenter', cancel);
+  // 
+  // addEvent(drop, 'drop', function (event) {
+  //   // stops the browser from redirecting off to the text.
+  //   if (event.preventDefault) {
+  //     event.preventDefault();
+  //   }
+  // 
+  //   this.innerHTML += '<p>' + event.dataTransfer.getData('Text') + '</p>';
+  // 
+  //   return false;
+  // });
+
+
+
+  // $(".drop").bind("drop", function(evt) {
+  //   alert(evt);
+  //   
+  // }) 
+
+})
