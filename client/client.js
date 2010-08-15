cbf42d4b09c14ab5da523ce88e31643ae7dda5cf
Brent Fitzgerald <burnto@gmail.com>
2010-08-14 17:23:25 -0700

    packaging up into taciturn var

--- client/client.js
+++ client/client.js
@@ -1,50 +1,60 @@
 // Set up websocket
 
-if(("WebSocket" in window)) {
-  var ws = new WebSocket("ws://localhost:8125");
-  
-  ws.onmessage = function(evt) {
-    console.log('onmessage' + evt);
-  };
-  
-  ws.onopen = function(evt) {
-    ws.send('oh hai');
-  };
-  
-  ws.onclose = function(evt) {
-    console.log('onclose' + evt);
-  };
-  
-  ws.onerror = function(evt) {
-    console.log('onerror' + evt);
+var taciturn = function() {
+  var ws;
+  var rcvHandler;
+
+
+  return {
+    init: function(rcv) {
+      rcvHandler = rcv;
+    },
+    
+    connect: function() {
+      if(("WebSocket" in window)) {
+        ws = new WebSocket("ws://localhost:8125");
+
+        ws.onmessage = function(evt) {
+          rcvHandler(evt);
+          console.log('onmessage' + evt);
+        };
+
+        ws.onopen = function(evt) {
+          ws.send('oh hai');
+        };
+
+        ws.onclose = function(evt) {
+          console.log('onclose' + evt);
+        };
+
+        ws.onerror = function(evt) {
+          console.log('onerror' + evt);
+        }
+      }else{
+        alert("This app requires websockets. Please use a browser that supports them!")
+      }
+    },
+
+    send: function() {
+      ws.send('oh hai');
+    },
+    
+    close: function() {
+      ws.close();
+    }
   }
-  ws.close(); 
-}else{
-  alert("This app requires websockets. Please use a browser that supports them!")
-}
+}();
+
+
 
 $(function() {
-  // Set up dropzone
+  taciturn.init(function(evt) {
+    console.log(evt);
+    $(".messages").html(evt);
+  })
   
-  function cancel(event) {
-    if (event.preventDefault) {
-      event.preventDefault();
-    }
-    return false;
-  }
-  
-  var drop = $(".drop");
-  drop.bind('dragover', function(evt) {
-    cancel(evt);
-  })
-  drop.bind('dragenter', function(evt) {
-    drop.find(".message").html("drop me here");
-    cancel(evt);
-  })
-  drop.bind('dragend', function(evt) {
-    cancel(evt);
-  })
-  drop.droppable('Files',
+  // Set up drop
+  $(".drop").droppable('Files',
 
         // Drag enter
         function(e) {
@@ -62,39 +72,5 @@ $(function() {
           $('ul', this).append('<li>' + e.dataTransfer.files[0].fileName + '</li>')
         }
     )
-  // 
-  // drop.bind('drop', function(evt) {
-  //   var files = evt.dataTransfer.files;
-  //   console.log(evt)
-  //   if(!files) {
-  //     for(var f in files) {
-  //       this.innerHTML += f.fileName;
-  //     }
-  //   }
-  //   // this.innerHTML += '<p>' + evt.dataTransfer.getData('Text') + '</p>';
-  //   cancel(evt);
-  // })
-  // 
-  // 
-  // addEvent(drop, 'dragover', cancel);
-  // addEvent(drop, 'dragenter', cancel);
-  // 
-  // addEvent(drop, 'drop', function (event) {
-  //   // stops the browser from redirecting off to the text.
-  //   if (event.preventDefault) {
-  //     event.preventDefault();
-  //   }
-  // 
-  //   this.innerHTML += '<p>' + event.dataTransfer.getData('Text') + '</p>';
-  // 
-  //   return false;
-  // });
-
-
-
-  // $(".drop").bind("drop", function(evt) {
-  //   alert(evt);
-  //   
-  // }) 
 
 })
